5b5d9a45f11271e89756eb964af1a7d0f648deca
judiciousmurich <murichjudicious45@gmail.com>
2025-01-06 23:52:07 +0300

    :feat add data

--- backend/patients/admin.py
+++ backend/patients/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Patient
 
-# Register your models here.
+class PatientAdmin(UserAdmin):
+    list_display = ('username', 'email', 'phone', 'is_staff')
+    search_fields = ('username', 'email', 'phone')
+    ordering = ('email',)
+
+admin.site.register(Patient, PatientAdmin)
